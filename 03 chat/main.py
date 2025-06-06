0a59431a4896db9e1a6ef4e68bfbed8045f83786
Nikhil-Dadhich <nikhil720416@gmail.com>
2025-06-07 02:28:58 +0530

    upadate 03 chat

--- 03 chat/main.py
+++ 03 chat/main.py
@@ -8,13 +8,14 @@ client = OpenAI(
   api_key=os.getenv("GEMINI_API_KEY"),
   base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
 )
-
+# zero shot promting
+SYSTEM_PROMPT = "You are a football expert and should answer questions about football not about other topics."
 response = client.chat.completions.create(
   model = 'gemini-1.5-flash',
   messages = [
     {
       "role": "system",
-      "content": "You are a football expert and should answer questions about football not about other topics."
+      "content": SYSTEM_PROMPT
     },
     {
       "role": "user",
